5f084ba3e80ce0f63bba7ac6126f980c841ea845
Alexter2003 <98928291+Alexter2003@users.noreply.github.com>
2022-04-07 20:03:41 -0600

    Delete src/proyecto1 directory

--- src/proyecto1/SuperAutoPets.java
+++
@@ -1,9 +0,0 @@
-package proyecto1;
-
-public class SuperAutoPets {
-
-    public static void main(String[] args) {
-        MotorDelJuego juego = new MotorDelJuego();
-        juego.Menu();
-    }
-}
